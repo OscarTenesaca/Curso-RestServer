218ec331990b3ed2902e547eff767672db586dde
Oscar Tenesaca <tenezacahenry.99@gmail.com>
2021-04-05 14:03:14 -0500

    Cascaron de de restServer

---
+++ controllers/usuarios.js
@@ -0,0 +1,69 @@
+const { response , request} = require('express');
+
+const usuariosGet=(req=request, res = response) => {
+
+    const { q, nombre= 'no name',apkey} = req.query;
+
+    res.json({
+        id: 'ok',
+        msg: 'api get- cpntrolador',
+        q,
+        nombre,
+        apkey
+    });
+}
+//  envian datos tipo json raw json body send
+// {
+//     "nombre": "Oscar T",
+//     "edad": 24,
+//     "id": 123,
+//     "apellido": "otra cosa"
+// }
+const usuariosPost=(req, res = response) => {
+    // const body= req.body;
+    const { nombre, edad }= req.body; // desestricturar para sacar solo los datos deseados 
+
+    res.status(201).json({
+        msg: 'api post- cpntrolador',
+        // body
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut=(req, res = response) => {
+    const { id } = req.params;
+    
+    res.json({
+        msg: 'api put- cpntrolador',
+        id
+    });
+}
+
+const usuariosPatch=(req, res = response) => {
+    res.json({
+        id: 'ok',
+        msg: 'api patch- cpntrolador'
+    });
+}
+
+const usuariosDelete=(req, res = response) => {
+    res.json({
+        id: 'ok',
+        msg: 'api delete- cpntrolador'
+    });
+}
+
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete,
+    
+
+}
---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath= '/api/usuarios'
+
+        // midelwares
+        this.middelwares();
+        // rutas de mi app
+        this.routes();
+
+    }
+
+    middelwares(){
+
+        //siempre cpnmfigurarlo si es fuera del local
+        this.app.use(cors());
+
+        //plectura y parceo de body
+        this.app.use( express.json() );
+
+        //direcctorio publico
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath , require('../routes/usuarios'));        
+    }
+
+    lisen(){
+
+        this.app.listen(this.port, ()=>{
+            console.log('servidor corriendo en el puerto',this.port)
+
+        });
+    }
+
+}
+
+module.exports = Server;
